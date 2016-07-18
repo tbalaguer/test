4a2fa01ca31e8fe94c0d04b851d26afba44828f7
tbalaguer <tbalaguer@live.com>
2016-07-17 23:21:04 -0700

    index

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>The HTML5 Herald</title>
+  <meta name="description" content="The ultimate HTML5 test">
+  <meta name="author" content="Thomas">
+
+  <link rel="stylesheet" href="css/styles.css">
+
+  <!--
+-->
+</head>
+
+<body>
+  <p>&nbsp</p>
+  <p>&nbsp</p>
+  <p>Hello Thomas</p>
+  <p>&nbsp</p>
+  <p>&nbsp</p>
+</body>
+</html>
